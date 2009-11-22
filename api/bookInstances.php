eb8a46c4e03772638537e36124e4785aba32ede9
williballenthin <willi.ballenthin@gmail.com>
2009-11-21 23:10:42 -0800

    a few more comments. tested error checking of class.php. should be pretty good.

--- api/bookInstances.php
+++ api/bookInstances.php
@@ -1,9 +1,11 @@
+<?
 /**
 * @file bookInstances.php
 * @author Willi Ballenthin
 * @date Nov, 2009
 *
 */
+?>
 
 <?php
   print '<?xml version="1.0" encoding="UTF-8" ?>';
--- api/class.php
+++ api/class.php
@@ -1,9 +1,11 @@
+<?
 /**
 * @file class.php
 * @author Willi Ballenthin
 * @date Nov, 2009
 *
 */
+?>
 
 <?php
   print '<?xml version="1.0" encoding="UTF-8" ?>';
