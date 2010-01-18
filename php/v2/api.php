0e196c6be623fc40e4142d03e9a119db0738d9bb
williballenthin <willi.ballenthin@gmail.com>
2010-01-18 15:03:14 -0800

    new stuff

---
+++ php/v2/api.php
@@ -0,0 +1,131 @@
+<?
+
+	function api_server_test() {
+		$test_file = $api_server_base_url . "test.php";
+		$test      = file_get_contents($test_file);
+		print $test;
+	}
+	
+	function api_method($method) {
+		$api_server_base_url = "http://localhost/09/ButlerBooks/";
+		$api_url = $api_server_base_url . "api/";
+		$url     = $api_url . $method;
+		$text    = file_get_contents($url);
+		$xml     = simplexml_load_string($text);
+		return $xml;
+	}
+
+	/**
+	* 	returns the newly created book_id on successful addition
+	* 	false otherwise
+	*/	
+	function add_book($title, $author, $description) {
+		$method  = "add/book.php?";
+		$method .= "title="   . urlencode($title);
+		$method .= "&author=" . urlencode($author);
+		$method .= "&description=" . urlencode($description);
+		
+		$xml = api_method($method);
+		if($xml->xpath('/response/error')) {
+			return false;
+		}
+		$arr = $xml->xpath('/response/success/book_id');
+		return $arr[0];
+	}
+	
+	/**
+	* 	returns the newly created book_version_id on successful addition
+	* 	false otherwise
+	*/
+	function add_version($book_id, $version, $isbn_10, $isbn_13) {
+		$method  = "add/bookVersion.php?";
+		$method .= "&book_id=" . urlencode($book_id);
+		$method .= "&version=" . urlencode($version);
+		$method .= "&isbn_10=" . urlencode($isbn_10);
+		$method .= "&isbn_13=" . urlencode($isbn_13);
+		
+		$xml = api_method($method);
+		if($xml->xpath('/response/error')) {
+			return false;
+		}
+		$arr = $xml->xpath('/response/success/book_version_id');
+		return $arr[0];
+	}
+	
+	/**
+	* 	returns the newly created book_instance_id on successful addition
+	* 	false otherwise
+	*/
+	function add_instance($book_version_id, $seller, $price) {
+		$method  = "add/bookInstance.php?";
+		$method .= "&book_version_id=" . urlencode($book_version_id);
+		$method .= "&seller=" . urlencode($seller);
+		$method .= "&price=" . urlencode($price);
+		
+		$xml = api_method($method);
+		if($xml->xpath('/response/error')) {
+			return false;
+		}
+		$arr = $xml->xpath('/response/success/book_instance_id');
+		return $arr[0];
+	}
+	
+	/**
+	*	applies a given field value from xml to an array/hash/map
+	* 	
+	*	$array should be a reference
+	*/
+	function array_field_from_xml($array, $xml, $field) {
+		$arr = $xml->xpath($field);
+		$array[$field] = $arr[0];
+	}
+	
+	/**
+	*	applies a given set of field values from xml to an array/hash/map
+	* 	
+	*	$array should be a reference
+	*/
+	function array_fields_from_xml($array, $xml, $fields) {
+		foreach($fields as $field) {
+			array_field_from_xml(&$array, $xml, $field);
+		}
+	}
+	
+	/**
+	* returns an array indexed by fields of a book instance
+	*/
+	function book_instance_array_from_xml($xml) {
+		$book_instance = array();
+		
+		$fields = array('title', 'author', 'description', 
+			'book_id', 'version', 'version_id', 'isbn_10', 
+			'isbn_13', 'seller', 'price', 'book_instance_id');
+		
+		array_fields_from_xml(&$book_instance, $xml, $fields);
+		return $book_instance;
+	}
+	
+	/**
+	*	returns an array of arrays, each sub array indexed by fields
+	* 	in a book_instance
+	*/
+	function book_instances($xml) {
+		$xml_book_instances = 
+				$xml->xpath('/response/result/book_instances/book_instance');
+	
+		$book_instances = array();
+		while(list( , $xml_book_instance) = each($xml_book_instances)) {
+							
+			array_push($book_instances, 
+						book_instance_array_from_xml($xml_book_instance));
+		}
+		return $book_instances;
+	}
+	
+	function get_all_book_instances() {
+		return book_instances(api_method("get/allBookInstances.php"));
+	}
+
+
+
+?>
---
+++ php/v2/index.php
@@ -0,0 +1,132 @@
+<html>
+	<head>
+
+		<style type="text/css">
+		
+		html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, font, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	outline: 0;
+	font-weight: inherit;
+	font-style: inherit;
+	font-size: 100%;
+	font-family: inherit;
+	vertical-align: baseline;
+}
+/* remember to define focus styles! */
+:focus {
+	outline: 0;
+}
+body {
+	line-height: 1;
+	color: black;
+	background: white;
+}
+ol, ul {
+	list-style: none;
+}
+/* tables still need 'cellspacing="0"' in the markup */
+table {
+	border-collapse: separate;
+	border-spacing: 0;
+}
+caption, th, td {
+	text-align: left;
+	font-weight: normal;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: "";
+}
+blockquote, q {
+	quotes: "" "";
+}
+		
+		
+		
+
+body {
+	background-color: gray;
+}
+
+#content {
+	width: 800px;
+	margin-left:auto;
+	margin-right:auto;
+	
+
+}
+
+#title {
+	font-family:helvetica;
+	width:100%;
+	text-align:center;
+	color: #69f;
+	font-size: 48px;
+	font-weight:bold;
+}
+
+.search_box {
+	float:left;
+	width: 300px;
+	margin:30px;
+	
+	padding:10px;
+	
+	-webkit-border-radius: 10px;
+	-moz-border-radius: 10px;
+	-o-border-radius: 10px;
+}
+
+.search_box input {
+	-webkit-border-radius: 5px;
+	-moz-border-radius: 5px;
+	-o-border-radius: 5px;
+}
+
+.search_box button {
+	float:right;
+}
+
+#buy_box {
+	background-color:#69F;
+}
+
+#sell_box {
+	background-color:#69F;
+}
+
+		</style>
+
+	</head>
+<body>
+
+<div id="content">
+	<h1 id="title">ButlerBooks<h1><br />
+
+	<div id="buy_box" class="search_box">	
+		<form action="buy_search.php" method="GET">
+			<input name="query" id="buy_input" />
+			<button>buy</button>
+		</form>
+	</div>
+
+
+	<div id="sell_box" class="search_box">
+		<form action="sell_search.php" method="GET">
+			<input name="query" id="sell_input" />
+			<button>sell</button>
+		</form>
+	</div>
+
+</div>	
+</body>
+</html>
